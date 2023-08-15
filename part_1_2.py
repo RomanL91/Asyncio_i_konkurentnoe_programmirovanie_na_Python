a157209e3114aad3544d0f343bb1954cd3aa0d1d
RomanL91 <dev_roman91@mail.ru>
2023-08-15 10:04:03 +0600

    листинг 1.1 пример ограничения производительности

---
+++ part_1_2.py
@@ -0,0 +1,13 @@
+# Что такое ограниченнесть производительностью ввода-вывода и ограниченность 
+# быстродействием процессора?
+
+# листинг 1.1
+import requests
+
+response = requests.get(url='https://www.google.com') # веб-запрос ограничен производительностью ввода-вывода
+items = response.headers.items()
+headers = [f'{key}: {header}' for key, header in items] # обработка ответа ограниченна быстродействием процессора
+formated_header = '\n'.join(headers) # конкатенация строк ограничена быстройдествием поцессора
+
+with open('headers.txt', 'w') as file:
+    file.write(formated_header) # запись на диск ограничена производительностью ввода-вывода
